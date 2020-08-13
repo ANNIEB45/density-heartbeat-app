6a4e5a3ecfb0d8dd3923ec31b11a222a6c57406f
ANNIEB45 <ANNIEBELLEFLEUR@GMAIL.com>
2020-08-13 16:19:59 -0400

    data not rendering on page. 404 error

--- client/src/component/Home.jsx
+++ client/src/component/Home.jsx
@@ -1,10 +1,42 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 export default class Home extends Component {
+    state = {
+        sensors: []
+    }
+
+    componentDidMount() {
+        this.getSensorData()
+    }
+
+    getSensorData = async () => {
+        try {
+            const res = await axios.get('api/v1/feed/')
+            const newState = { ...this.state }
+            newState.sensors = res.data
+            this.setState(newState)
+        } catch (err) {
+            console.log('Error getting all sensors')
+            console.log(err)
+        }
+    }
+
+// data not rendering on page??? 
+    // getting status 404 error
     render() {
         return (
             <div>
                 <h1>Home Page</h1>
+                { this.state.sensors.map((sensor) => {
+                    return (
+                        <div>
+                            { sensor.name }
+                            { sensor.location }
+                            { sensor.heartbeat }
+                        </div>
+                    )
+                }) }
             </div>
         )
     }
