f5eb4d265f63ad061276b5d0cb59023fe871f4a4
ANNIEB45 <ANNIEBELLEFLEUR@GMAIL.com>
2020-08-13 14:33:03 -0400

    bug in url file, cannot import view, will fix later

--- density_heartbeat_project/urls.py
+++ density_heartbeat_project/urls.py
@@ -13,12 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.urls import path, include, re_path
 from django.contrib import admin
-from django.urls import path
+
+# bug-File "/Users/annebellefleur/density_assignment/density_heartbeat_project/urls.py", line 19, in <module>
+#     from . import views
+# ImportError: cannot import name 'views' from 'density_heartbeat_project' (/Users/annebellefleur/density_assignment/density_heartbeat_project/__init__.py)
+    # can't figure out proplem
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.FrontendAppView.as_view()),
     path('api/v1/', include('density_heartbeat_app.urls')),
     re_path(r'^.*$', views.FrontendAppView.as_view()),
     
 ]
+
