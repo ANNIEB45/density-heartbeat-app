e7262f91a0c128bdcf4cf61613a1c3ab694289e2
ANNIEB45 <ANNIEBELLEFLEUR@GMAIL.com>
2020-08-12 23:19:00 -0400

    created models for heartbeat and sensor

--- density_heartbeat_app/models.py
+++ density_heartbeat_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+# Creating models for heartbeat and sensor
+# Heartbeat is a child of sensor
+
+# Sensor model fields
+    # alive-boolean
+    # checking in-boolean, timestamp
+class Sensor(models.Model):
+    name = models.CharField(max_length=250)
+    location = models.CharField(max_length=250) 
+    isalive = models.CharField(max_length=250)
+    check_in = models.CharField(max_length=250)
+
+
+# Heartbeat model fields
+    # beats- foreignkey
+class Heartbeat(models.Model):
+    rhythm = models.ForeignKey(Sensor, on_delete=models.CASCADE, related_name='heartbeat')
+
